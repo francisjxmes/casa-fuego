f41580d77d882803481f97182ab0f81504ed6817
francisjxmes <francissalinger25@gmail.com>
2025-11-23 15:22:15 +0000

    Added Django views and URLs for index, menu, and booking pages

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "bookings/index.html")
+
+def menu(request):
+    return render(request, "bookings/menu.html")
+
+def booking(request):
+    return render(request, "bookings/booking.html")
