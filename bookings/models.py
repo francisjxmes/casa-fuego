a2374eca8663e9bf31cb238a7f165a4d20645490
francisjxmes <francissalinger25@gmail.com>
2025-11-23 15:36:55 +0000

    Added booking model and applied migrations

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=50)
+    party_size = models.PositiveIntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.date} @ {self.time}"
+
+
