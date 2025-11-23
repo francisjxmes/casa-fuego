6868bf1aba51be34b21b8642f263321274068ce0
francisjxmes <francissalinger25@gmail.com>
2025-11-23 17:26:26 +0000

    Styled login and signup pages to match booking form

--- casa_fuego/settings.py
+++ casa_fuego/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'casa_fuego.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+  BASE_DIR / "bookings/static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
